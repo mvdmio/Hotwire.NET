5372a7eb2da43e2fe584a3ab127087e58ae86c2f
Michiel van der Meer <contact@michielvandermeer.com>
2023-06-02 22:57:07 +0200

    Added Turbo Frame example

---
+++ Example.ASP.NetCore.MVC/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.ASP.NetCore.MVC.Controllers;
+
+[Route("")]
+public class HomeController : Controller
+{
+   [Route("")]
+   public IActionResult Index()
+   {
+      return View();
+   }
+}
---
+++ Example.ASP.NetCore.MVC/Controllers/TurboFramesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.ASP.NetCore.MVC.Controllers;
+
+[Route("example/turbo-frames")]
+public class TurboFramesController : Controller
+{
+   [Route("")]
+   public IActionResult Index()
+   {
+      return View();
+   }
+
+   [Route("content")]
+   public IActionResult Content()
+   {
+      return PartialView("_Content");
+   }
+}
