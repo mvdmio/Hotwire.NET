420ed2b70c46ca59c312226b1baeab2555fea661
Michiel van der Meer <contact@michielvandermeer.com>
2024-08-16 20:52:52 +0200

    Added TurboStreams example

---
+++ Example.ASP.NetCore.MVC/Controllers/TurboStreamsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvdmio.Hotwire.NET.ASP.Extensions;
+using mvdmio.Hotwire.NET.ASP.TurboActions;
+
+namespace Example.ASP.NetCore.MVC.Controllers;
+
+[Route("example/turbo-streams")]
+public class TurboStreamsController : Controller
+{
+   [Route(""), HttpGet, HttpPost]
+   public async Task<IActionResult> Index()
+   {
+      if (Request.IsTurboRequest())
+      {
+         var contentPartial = await this.RenderView("_Content", DateTime.Now);
+         return this.TurboStream(new ReplaceTurboAction("content", contentPartial));
+      }
+
+      return View();
+   }
+}
---
+++ Example.ASP.NetCore.MVC/Views/TurboStreams/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Example.ASP.NetCore.MVC.Views.TurboStreams;
+
+public class Index : PageModel
+{
+   public void OnGet()
+   {
+      
+   }
+}
