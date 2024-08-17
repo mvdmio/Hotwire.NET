6ca2ac838721ca3ca3c45045163e75a86f7f7c2f
Michiel van der Meer <contact@michielvandermeer.com>
2024-08-17 10:53:01 +0200

    Added Razor Pages example

---
+++ Example.ASP.NetCore.RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Example.ASP.NetCore.RazorPages.Pages;
+
+public class IndexModel : PageModel
+{
+   public void OnGet()
+   {
+   }
+}
---
+++ Example.ASP.NetCore.RazorPages/Pages/TurboStreams/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using mvdmio.Hotwire.NET.ASP.Extensions;
+using mvdmio.Hotwire.NET.ASP.TurboActions;
+
+namespace Example.ASP.NetCore.RazorPages.Pages.TurboStreams;
+
+public class Index : PageModel
+{
+   public IActionResult OnGet()
+   {
+      return Page();
+   }
+
+   public async Task<IActionResult> OnPost()
+   {
+      if (Request.IsTurboRequest())
+      {
+         var renderedPartial = await this.RenderView("_Content", DateTime.Now);
+         return this.TurboStream(new ReplaceTurboAction("content", renderedPartial));
+      }
+
+      return Page();
+   }
+}
