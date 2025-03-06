c9204478222610fcfaa152341ad6387fbcc07260
Michiel van der Meer <mvdmeer@jewelsoftware.com>
2025-03-06 10:24:57 +0100

    Added logging to websockets middleware

--- mvdmio.Hotwire.NET/ASP/Broadcasting/TurboStreamsWebsocketController.cs
+++ mvdmio.Hotwire.NET/ASP/Broadcasting/TurboStreamsWebsocketController.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using mvdmio.Hotwire.NET.ASP.Broadcasting.Interfaces;
 
 namespace mvdmio.Hotwire.NET.ASP.Broadcasting;
@@ -13,14 +14,16 @@ public class TurboStreamsWebsocketMiddleware : IMiddleware
 {
    private readonly ITurboBroadcaster _broadcaster;
    private readonly IChannelEncryption _channelEncryption;
+   private readonly ILogger<TurboStreamsWebsocketMiddleware> _logger;
 
    /// <summary>
    ///   Constructor.
    /// </summary>
-   public TurboStreamsWebsocketMiddleware(ITurboBroadcaster broadcaster, IChannelEncryption channelEncryption)
+   public TurboStreamsWebsocketMiddleware(ITurboBroadcaster broadcaster, IChannelEncryption channelEncryption, ILogger<TurboStreamsWebsocketMiddleware> logger)
    {
       _broadcaster = broadcaster;
       _channelEncryption = channelEncryption;
+      _logger = logger;
    }
 
    /// <inheritdoc />
@@ -32,32 +35,39 @@ public class TurboStreamsWebsocketMiddleware : IMiddleware
          return;
       }
 
-      if (!context.Request.Path.StartsWithSegments("/turbo/ws"))
-      {
-         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-         return;
-      }
-      
-      var signedChannelName = context.Request.Path.Value?.Substring("/turbo/ws/".Length);
-      if(string.IsNullOrWhiteSpace(signedChannelName))
-      {
-         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-         return;
-      }
-
       try
       {
-         var channelName = _channelEncryption.Decrypt(signedChannelName);
+         if (!context.Request.Path.StartsWithSegments("/turbo/ws"))
+         {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return;
+         }
 
-         var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-         var tcs = new TaskCompletionSource();
-         await _broadcaster.AddConnection(channelName, webSocket, tcs);
+         var signedChannelName = context.Request.Path.Value?.Substring("/turbo/ws/".Length);
+         if (string.IsNullOrWhiteSpace(signedChannelName))
+         {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return;
+         }
 
-         await tcs.Task;
+         try
+         {
+            var channelName = _channelEncryption.Decrypt(signedChannelName);
+
+            var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            var tcs = new TaskCompletionSource();
+            await _broadcaster.AddConnection(channelName, webSocket, tcs);
+
+            await tcs.Task;
+         }
+         catch (ArgumentException) // Thrown by the channel encryption when signature is not valid.
+         {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+         }
       }
-      catch (ArgumentException) // Thrown by the channel encryption when signature is not valid.
+      catch (Exception ex)
       {
-         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+         _logger.LogError(ex, "Error in TurboStreamsWebsocketMiddleware");
       }
    }
 }
